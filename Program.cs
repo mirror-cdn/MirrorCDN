59098f2f625fc80965eccab402fcf0e2725f2d61
MIAIONE <MIAIONE@Yeah.net>
2022-02-17 19:41:27 +0800

    new-src

--- Program.cs
+++ Program.cs
@@ -80,7 +80,7 @@ public static class Ext
         {
             byte[] rawData = System.Text.Encoding.UTF8.GetBytes(rawString.ToString());
             byte[] zippedData = Compress(rawData);
-            return (string)(Convert.ToBase64String(zippedData));
+            return (string)(Convert.ToBase64String(zippedData).Replace("/","-"));
         }
     }
 
@@ -106,7 +106,7 @@ public static class Ext
     public static string GzipToString(string Value)
     {
         //DataSet ds = new DataSet();
-        string CC = GZipDecompressString(Value);
+        string CC = GZipDecompressString(Value.Replace("-", "/"));
         //System.IO.StringReader Sr = new System.IO.StringReader(CC);
         //ds.ReadXml(Sr);
         return CC;
@@ -152,10 +152,6 @@ public static class Ext
 
     }
 
-    public static void SetCookice()
-    {
-      
-
-    }
+   
 
 } 
